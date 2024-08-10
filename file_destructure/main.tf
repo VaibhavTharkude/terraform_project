4b858316518f4e053131c52ae306b57ab9307b8e
Vaibhav T\ Tharkude <tharkudev9007@gmail.comgit config --global user.email tharkudev9007@gmail.com>
2024-08-10 10:54:46 +0530

    --- Auto Git Commit ---

--- file_destructure/main.tf
+++ file_destructure/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "this" {
   ami                     = var.ami
   instance_type           = var.instance_type
   key_name                = var.key_name
-  instance
+  instance_name
 
   tags = var.tags
 
