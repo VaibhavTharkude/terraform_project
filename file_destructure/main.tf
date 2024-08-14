44a9b37871bef35eb394bfc9eead96c69a9fd993
Vaibhav T\ Tharkude <tharkudev9007@gmail.comgit config --global user.email tharkudev9007@gmail.com>
2024-08-14 20:46:04 +0530

    --- Auto Git Commit ---

--- file_destructure/main.tf
+++ file_destructure/main.tf
@@ -1,24 +1,58 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.61.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.61.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = var.region
+}
+
+resource "aws_key_pair" "my_key" {
+    key_name = "terra_key"
+    public_key = file("~/.ssh/id_rsa.pub")
+}
+
+locals {
+  map = {
+    "HTTP" = {
+      port = 80,
+      cidr_blocks = ["0.0.0.0/0"],
+    }
+    "SSH" = {
+      port = 22,
+      cidr_blocks = ["10.0.0.0/16"],
+    }
+    "HTTPS" = {
+      port = 443,
+      cidr_blocks = ["0.0.0.0/0"],
     }
   }
 }
 
-provider "aws" {
-  # Configuration options
-  region = var.region
+resource "aws_security_group" "mysg" {
+  name        = "webserver"
+  description = "Inbound Rules for WebServer"
+
+  dynamic "ingress" {
+    for_each = local.map
+    content {
+      description = ingress.key
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
 }
 
-##### Try to create one ubuntu server
-resource "aws_instance" "this" {
-  ami                     = var.ami
-  instance_type           = var.instance_type
-  key_name                = var.key_name
-  
-  tags = var.tags
-
-}
-
+# resource "aws_instance" "this" {
+#     ami           = var.ami
+#     instance_type = var.instance_type
+#     key_name      = aws_key_pair.my_key.name.id
+#     vpc_security_group_ids = ["${aws_security_group.mysg.id}"]
+#     tags          = var.tags
+# }
