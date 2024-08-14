f6dfc71d94ad72542964e35d7275289f54f2f871
Vaibhav T\ Tharkude <tharkudev9007@gmail.comgit config --global user.email tharkudev9007@gmail.com>
2024-08-15 01:05:24 +0530

    --- Auto Git Commit ---

--- S3_bucket/main.tf
+++ S3_bucket/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform-state" {
-    bucket = "terraform-state-bucket-cdec-"
+    bucket = "terraform-state-bucket-cdec-vaibhav"
 }
 
 resource "aws_s3_bucket_versioning" "terraform-state" {
