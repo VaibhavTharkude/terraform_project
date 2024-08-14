1c0ad6cfd21b7414598c622b953de391a38620ba
Vaibhav T\ Tharkude <tharkudev9007@gmail.comgit config --global user.email tharkudev9007@gmail.com>
2024-08-15 01:16:37 +0530

    --- Auto Git Commit ---

--- S3_bucket/main.tf
+++ S3_bucket/main.tf
@@ -10,15 +10,15 @@ resource "aws_s3_bucket_versioning" "terraform-state" {
     }
 }
 
-# resource "aws_dynamodb_table" "state_lock_table" {
-#     name = "terraform_state_lock"
-#     billing_mode = "PAY_PER_REQUEST"
-#     hash_key = "LockID"
-#     attribute {
-#         name = "LockID"
-#         type = "S"
-#     }
-# }
+resource "aws_dynamodb_table" "state_lock_table" {
+    name = "terraform_state_lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
 
 resource "aws_instance" "server_1" {
     ami = var.ami
