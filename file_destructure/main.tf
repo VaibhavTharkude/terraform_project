76ff80ace3f14f69c013c05ece01f3686bb740f6
Vaibhav T\ Tharkude <tharkudev9007@gmail.comgit config --global user.email tharkudev9007@gmail.com>
2024-08-14 21:31:08 +0530

    --- Auto Git Commit ---

--- file_destructure/main.tf
+++ file_destructure/main.tf
@@ -54,7 +54,7 @@ resource "aws_key_pair" "my_key" {
 resource "aws_instance" "this" {
     ami           = var.ami
     instance_type = var.instance_type
-    key_name      = aws_key_pair.my_key.name
+    key_name      = aws_key_pair.my_key.
     vpc_security_group_ids = ["${aws_security_group.mysg.id}"]
     tags          = var.tags
 }
