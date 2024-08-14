2388df9c8bc1e2a98baac653134cd11ede9bbf1f
Vaibhav T\ Tharkude <tharkudev9007@gmail.comgit config --global user.email tharkudev9007@gmail.com>
2024-08-14 23:57:13 +0530

    --- Auto Git Commit ---

---
+++ setup/variable.tf
@@ -0,0 +1,24 @@
+variable "region" {
+    type = string
+    description = "region calling"
+}
+
+variable "ami" {
+    type = string
+    description = "ami calling"
+}
+
+variable "instance_type" {
+    type = string
+    description = "instance_type calling"
+}
+
+variable "key_name" {
+    type = string
+    description = "key_pair calling"
+}
+
+variable "tags" {
+    type = map
+}
+
