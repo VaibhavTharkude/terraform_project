da9b77fcfe2b7b31222c4071c3f182524bc749ed
Vaibhav T\ Tharkude <tharkudev9007@gmail.comgit config --global user.email tharkudev9007@gmail.com>
2024-08-15 20:29:40 +0530

    --- Auto Git Commit ---

--- S3_bucket/provider.tf
+++ S3_bucket/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
    backend "s3" {
-       bucket = "buccket-14235"
+       bucket = "terraform-state-bucket-cdec-vaibhav"
        key = "terraform.tfstate"
        region = "ap-south-1"
        dynamodb_table = "terraform_state_lock"
